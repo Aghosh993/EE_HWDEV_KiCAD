eeedfc113edbb615646c9440b407e09ad142593e
Aghosh993 <Aghosh993@gmail.com>
2016-11-25 15:04:59 -0500

    Adding simple Arduino for PolyBOTS workshop.

---
+++ damn_simple_arduino/Outputs/make_4pcb_zip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./rename.sh
+zip -r gerbers.zip tmp/
+rm -r tmp/
+
---
+++ damn_simple_arduino/Outputs/rename.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir tmp/
+cp Gerbers/damn_simple_arduino-F.SilkS.gbr tmp/damn_simple_arduino.GTO
+cp Gerbers/damn_simple_arduino-B.SilkS.gbr tmp/damn_simple_arduino.GBO
+cp Gerbers/damn_simple_arduino-F.Cu.gbr tmp/damn_simple_arduino.GTL
+cp Gerbers/damn_simple_arduino-B.Cu.gbr tmp/damn_simple_arduino.GBL
+cp Gerbers/damn_simple_arduino-F.Mask.gbr tmp/damn_simple_arduino.GTS
+cp Gerbers/damn_simple_arduino-B.Mask.gbr tmp/damn_simple_arduino.GBS
+cp Gerbers/damn_simple_arduino.drl tmp/damn_simple_arduino.DRL
+cp Gerbers/damn_simple_arduino-Edge.Cuts.gbr tmp/damn_simple_arduino.DWG
+
